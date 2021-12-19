e57dbb4af4f7deff13ba2db33eb00ce454c1db89
khatuantran <khatuantran11@gmail.com>
2021-12-19 12:10:27 +0700

    read slang word and create menu form

--- src/dict_19127429/Slangword.java
+++ src/dict_19127429/Slangword.java
@@ -4,10 +4,56 @@
  */
 package dict_19127429;
 
+
+import java.io.*;
+import java.util.*;
+
 /**
  *
  * @author jame
  */
 public class Slangword {
+    HashMap<String, Vector<String>> SlangwordList;
+    String path = ".//slang.txt";
+    
+    public Slangword() {
+        SlangwordList = new HashMap<>();
+        readFile(path);
+    }
+    
+    public void readFile(String path)
+    {
+        try
+        {            
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(path))); 
+            String str;
+            while((str = br.readLine()) != null)
+            {
+                String Slangword[] = str.split("`");
+                String temp[] = Slangword[1].split("| ");
+//                Slangword[1].pl
+                Vector<String> meaning = new Vector<>();
+                for(int i = 0; i < temp.length; i++)
+                {
+                    meaning.add(temp[i]);
+                }
+                
+                SlangwordList.put(Slangword[0], meaning);
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public static void main(String[] args) {
+//        new Slangword();
+//        String a ="abc";
+//        String b[] = a.split(". ");
+//        a.
+//        System.out.println(b.length);
+//        System.out.println(a.split(".").length);
+    }
     
 }
